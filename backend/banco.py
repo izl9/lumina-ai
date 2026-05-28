02fb6d673d528a2c5651c3eef4556c9c02de5980
izl <izl.dev999@gmail.com>
2026-05-27 21:43:47 -0300

    create sqlite tables automatically

--- backend/banco.py
+++ backend/banco.py
@@ -3,3 +3,23 @@ import sqlite3
 bd = sqlite3.connect("Lumina.bd", check_same_thread=False)
 
 cursor = bd.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT,
+    email TEXT,
+    senha TEXT
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS conversas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    usuario_id INTEGER,
+    pergunta TEXT,
+    resposta TEXT
+)
+""")
+
+conexao.commit()
