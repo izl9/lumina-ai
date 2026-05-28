cb5f67e1b14855688575cadf9593a05ab00102a1
izl <izl.dev999@gmail.com>
2026-05-27 21:59:46 -0300

    fix sqlite tables

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,9 @@ from flask import Flask
 from dotenv import load_dotenv
 import os
 
+print("🔥 APP INICIANDO")
+print("DB PATH:", os.path.abspath("Lumina.bd"))
+
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
 load_dotenv()
@@ -11,5 +14,4 @@ app.secret_key = os.getenv("APP_SECRET_KEY")
 from backend.views import *
 
 if __name__ == "__main__":
-    print("DB PATH:", os.path.abspath("Lumina.bd"))
     app.run()
